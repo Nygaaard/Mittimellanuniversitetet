85e5a583eb0535c4e8b94f190a8158ed7812f936
Nygaaard <anny2301@student.miun.se>
2024-05-24 09:06:16 +0200

    Get and display courses

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { HomepageCourse } from '../models/homepage-course';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  //Properties
+  courses: HomepageCourse[] = [];
 
+  constructor(private courseservive: CoursesService) {}
+
+  ngOnInit() {
+    this.courseservive.getCourses().subscribe((courses) => {
+      this.courses = courses;
+    });
+  }
 }
---
+++ src/app/models/homepage-course.ts
@@ -0,0 +1,7 @@
+export interface HomepageCourse {
+  courseCode: string;
+  courseName: string;
+  points: string;
+  subject: string;
+  syllabus: string;
+}
